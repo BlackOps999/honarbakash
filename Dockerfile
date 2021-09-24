b7d5dadac2bb7150601ebe060ba3ade545d948c0
Adam Black <princeads@hotmail.com>
2021-09-24 23:08:52 +0100

    include build

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@
 FROM node:14
 WORKDIR /app
 COPY package*.json ./
-RUN npm install
-#RUN npm ci --only=production
+#RUN npm install
+RUN npm ci --only=production
 COPY . .
 EXPOSE 8080
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
   
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
