c3f8059709e39da353c467f345ea8c68f9cb05c1
Adam Black <adam.ross.black@gmail.com>
2021-10-03 13:11:33 +0100

    updates

---
+++ db-schema.sql
@@ -0,0 +1,10 @@
+-- "ABTest"."AB-Navigation" definition
+
+-- Drop table
+
+-- DROP TABLE "ABTest"."AB-Navigation";
+
+CREATE TABLE "ABTest"."AB-Navigation" (
+	id int4 NULL,
+	titles varchar NULL
+);
