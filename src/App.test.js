fb97d90e2f87af23bee9b7ed41ff35e694b987f2
Adam Black <princeads@hotmail.com>
2022-08-02 14:05:09 +0100

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 test('Home button rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/home/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('about rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/about/i);
+  const linkElement = screen.getByText(/Bio/i);
   expect(linkElement).toBeInTheDocument();
 });
