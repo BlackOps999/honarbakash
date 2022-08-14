179b85127d420acf057f5e26fb1a7552084891e5
Adam Black <princeads@hotmail.com>
2022-08-14 12:03:54 +0100

    Add database routes for publications

--- server.js
+++ server.js
@@ -3,12 +3,79 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const cors = require("cors");
+const pool = require("./db"); //run queries on postgres
+const { AppBar } = require('@material-ui/core');
 
 //middleware
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
+app.use(cors());
 
 console.log(path.join(__dirname, 'build'));
+
+// BACKEND ROUTES //
+
+// Create Publication
+app.post("/api-createpublication", async(req, res) => {
+  try {
+    const {article} = req.body;
+    const newPublication = await pool.query("INSERT INTO publications (article) VALUES($1) RETURNING *", [article]);
+    
+    res.json(newPublication.rows[0]);
+    console.log(req.body);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get all Publications
+app.get("/api-getpublications", async(req, res) => {
+  try{  
+    const allPublications = await pool.query("SELECT * FROM publications");
+    res.json(allPublications.rows);
+  }catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get a Publication
+app.get("/api-getpublications/:id", async(req, res) => {
+  try {
+    const {id} = req.params;
+    const publication = await pool.query("SELECT * FROM publications WHERE pub_id = $1", [id]);
+    
+    res.json(publication.rows[0]);
+
+    console.log(req.params);
+  } catch (err) {
+    console.error(err.message);
+  };
+});
+
+// Update a Publication
+app.put("/api-updatepublication/:id", async(req, res) => {
+  try {
+    const {id} = req.params;
+    const {article} = req.body;
+    const updatePublication = await pool.query("UPDATE publications SET article = $1 WHERE pub_id = $2", [article, id]);
+  
+    res.json("Publication Updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Delete a Publication
+app.delete("/api-deletepublications/:id", async(req, res) => {
+  try {
+    const {id} = req.params;
+    const deletePublication = await pool.query("DELETE FROM publications WHERE pub_id = $1", [id]);
+
+    res.json("Publication deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
