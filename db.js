8b71c768c7846365614c61443c422ad368d32948
Adam Black <princeads@hotmail.com>
2022-08-17 18:50:13 +0100

    changed to host db

--- db.js
+++ db.js
@@ -1,17 +1,35 @@
+const Pool = require("pg").Pool;
 const Knex = require('knex');
+require("dotenv").config();
 
-const pool = async config => {
+const devConfig = {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT
+};
+
+const proConfig = async config => {
     return Knex({
         client: 'pg',
-        debug: true,
         connection: {
-            user: 'postgres',
+            user: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
-            database: 'sherry',
-            socketPath: '/cloudsql/noble-nation-321921:europe-west4:ablackmagic-db/.s.PGSQL.5432'
+            database: process.env.POSTGRES_DB,
+            host: `/cloudsql/${process.env.POSTGRES_HOST}/.s.PGSQL.5432`
         },
         options: {}
     });
 };
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
+
+console.log(process.env.POSTGRES_USER);
+console.log(process.env.POSTGRES_PASSWORD);
+console.log(process.env.POSTGRES_DB);
+console.log(`/cloudsql/${process.env.POSTGRES_HOST}`);
+
+console.log(process.env.NODE_ENV);
 
 module.exports = pool;
