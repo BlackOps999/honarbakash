2e1d5d02d206ffce1b4d75f581d114e9f35e89a3
Adam Black <princeads@hotmail.com>
2022-08-17 21:18:20 +0100

    remove knex

--- db.js
+++ db.js
@@ -1,34 +1,16 @@
 const Pool = require("pg").Pool;
-const Knex = require('knex');
 require("dotenv").config();
 
 const devConfig = {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT
+    host: `/cloudsql/${process.env.POSTGRES_HOST}`
 };
 
-const proConfig = async config => {
-    return Knex({
-        client: 'pg',
-        connection: {
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB,
-            host: `/cloudsql/${process.env.POSTGRES_HOST}`
-        },
-        options: {}
-    });
-};
+const pool = new Pool(devConfig);
 
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
-
-console.log(process.env.POSTGRES_USER);
-console.log(process.env.POSTGRES_PASSWORD);
-console.log(process.env.POSTGRES_DB);
-console.log(`/cloudsql/${process.env.POSTGRES_HOST}`);
+console.log(devConfig);
 
 console.log(process.env.NODE_ENV);
 
