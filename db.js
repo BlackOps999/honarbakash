12ef5a1210aae804c6e6a582a8b4d132eb9f8b94
Adam Black <princeads@hotmail.com>
2022-08-17 18:43:41 +0100

    debug

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ const Knex = require('knex');
 const pool = async config => {
     return Knex({
         client: 'pg',
+        debug: true,
         connection: {
-            user: postgres,
+            user: 'postgres',
             password: process.env.POSTGRES_PASSWORD,
-            database: sherry,
-            socketPath: `/cloudsql/noble-nation-321921:europe-west4:ablackmagic-db/.s.PGSQL.5432`
+            database: 'sherry',
+            socketPath: '/cloudsql/noble-nation-321921:europe-west4:ablackmagic-db/.s.PGSQL.5432'
         },
         options: {}
     });
