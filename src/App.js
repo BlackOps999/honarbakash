0ad06aac14bf81ab9c99d93a9c100c97365a189d
Adam Black <princeads@hotmail.com>
2022-08-21 12:14:42 +0100

    Fix all tests

--- src/App.js
+++ src/App.js
@@ -10,48 +10,35 @@ import ContactMe from './components/pages/ContactMe';
 import RhythmAI from './components/pages/RhythmAI';
 import Services from './components/pages/Services';
 import jwt_decode from "jwt-decode";
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import {gapi} from 'gapi-script';
 
 function App() {
 
   const [user, setUser] = useState({});
-
-  function handleCallbackResponse(response){
-      //console.log("Encoded JWT token ID: " + response.credential);
-      const userObject = jwt_decode(response.credential);
-      //console.log(userObject);
-      setUser(userObject);
-      // user logged in, hide the login button ***WILL WORK WHEN MOVE setUser to global variable
-      //document.getElementById("signInDiv").hidden = true;
-  };
-
-  function handleSignOut(event) {
-    setUser({});
-    // user logged out, hide the logout button and show login ***WILL WORK WHEN MOVE setUser to global variable
-    document.getElementById("btn-signOut").hidden = true;
-    //console.log("User: " && user);
-  };
+  const clientId = '541789097928-fhs8jv38r9jaqjfe4527ofp4tg8vqjpq.apps.googleusercontent.com';
 
   useEffect(() => {
-    /* global google script from index.html */
-    google.accounts.id.initialize({
-      client_id: "541789097928-fhs8jv38r9jaqjfe4527ofp4tg8vqjpq.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    });
+    const initClient = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  });
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      {theme: "filled_white", size: "large", shape: "circle", type: "icon"}
-    );
+  const onSuccess = (res) => {
+    console.log('Success: ', res);
+  };
 
-    google.accounts.id.prompt();
-
-  }, []);
+  const onFailure = (err) => {
+    console.log('Failed: ', err);
+  };
 
   return (
     <> 
-      {Object.keys(user).length != 0 &&
-        <button id="btn-signOut" onClick={(e) => handleSignOut(e)}>Sign Out</button>
-      }
+      <GoogleLogin data-shape="circular" data-size="large" data-type="icon" data-theme="filled_white" clientId={clientId} onSuccess={onSuccess} onFailure={onFailure} cookiePolicy={'single_host_origin'} isSignedIn={true} />
       <Router>
         <Navbar />
         <Routes>
