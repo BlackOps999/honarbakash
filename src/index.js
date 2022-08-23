8c136003b97e6fba41690481c447c110f78de246
Adam Black <princeads@hotmail.com>
2022-08-23 09:56:06 +0100

    remove OLD google react-oauth/google package

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {GoogleOAuthProvider} from '@react-oauth/google';
 
 ReactDOM.render(
-  <GoogleOAuthProvider clientId="541789097928-fhs8jv38r9jaqjfe4527ofp4tg8vqjpq.apps.googleusercontent.com">
-    <App />
-  </GoogleOAuthProvider>,
+    <App />,
   document.getElementById('root')
 );
 
