07bc287fe12ba28074744b38f55c07fa96bd2cd9
Adam Black <princeads@hotmail.com>
2022-10-04 21:40:38 +0100

    Bio changes

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -11,6 +11,7 @@ import employmentBg from '../../images/bio/employment-bg.png';
 function Bio() {
     return (
         <>
+        <div className='bio'>
             <div className='bio-container'>
                 <div className='bio-intro-items'>
                     <h1 className='h1-bio-intro'>“My experience and research is here to help you reach your life goals. The breadth and diversity of my experience make we well positioned to achieve this.
@@ -54,20 +55,21 @@ function Bio() {
                 <div className='bio-content-cell3'>
                     <h1>Qualifications:</h1>                
 
-                    PhD 2016-2019
-                    Queen Mary University of London, November 2019MRCP paces- February 2012
-                    MRCP part 2- April 2011
-                    MRCP part 1- September 2010
-                    MBBS Honours 2003-2009
-
-                    Distinction in Medical Sciences
-                    Distinction in Clinical Sciences
-                    Imperial College London, July 2009
-
-                    Intercalated Cardiovascular BSc
-                    1st Class Honours
-                    Imperial College London, September 2006-2007
-
+                    <p><b>PhD 2016-2019</b><br />
+                    Queen Mary University of London, November 2019MRCP paces- February 2012</p>
+                    <br />
+                    <p><b>MRCP part 2- April 2011<br />
+                    MRCP part 1- September 2010<br />
+                    MBBS Honours 2003-2009</b></p>
+                    <br />
+                    <p><b>Distinction in Medical Sciences<br />
+                    Distinction in Clinical Sciences</b><br />
+                    Imperial College London, July 2009</p>
+                    <br />
+                    <p><b>Intercalated Cardiovascular BSc</b><br />
+                    1st Class Honours<br />
+                    Imperial College London, September 2006-2007</p>
+                    <br />
                     4 A-levels achieved with grade A (Mathematics, Physics, Biology and Chemistry). Thomas Tallis Sixth form school. Kidbrooke, London
                 </div>
                 <div className='bio-content-cell4'><img class='educationBg' src={educationBg}></img><img class='education' src={education}></img></div>
@@ -75,24 +77,25 @@ function Bio() {
                 <div className='bio-content-cell6'>
                     <h1>Employment:</h1>
 
-                    BHF Clinical Intermediate Fellowship
-                    Awarded March 2022, awaiting ethics approval
-
-                    Post CCT Clinical Fellow in Cardiology and Electrophysiology
-                    October 2021. Barts Heart Centre, Barts Health NHS trust 
-
-                    Founder and Chief Technology Officer, Rhythm AI Ltd		
-                    June 2019 – present. Having invented, patented and published on a technique to map AF called ‘STAR mapping’ I founded a company together with my co-inventors to take STAR forward. This has received £2.2m in funding from Venture Capital and will fund a multi-centre trial. https://rhythm-ai.com/
-
-                    British Heart Rhythm Society Multicentre Trial Group 				
-                    Board member, fellow representative. October 2020- present
-
-                    Honorary Clinical Lecturer at Queen Mary University of London. June 2020- present
-
-                    Associate Editor of European Heart Journal Case reports. August 2020-present
-                    This is a sub-journal of the European Heart Journal focusing on Case Reports.
+                    <p><b>BHF Clinical Intermediate Fellowship</b><br />
+                    Awarded March 2022, awaiting ethics approval</p>
+                    <br />
+                    <p><b>Post CCT Clinical Fellow in Cardiology and Electrophysiology</b><br />
+                    October 2021. Barts Heart Centre, Barts Health NHS trust </p>
+                    <br />
+                    <p><b>Founder and Chief Technology Officer, Rhythm AI Ltd</b><br />		
+                    June 2019 – present. Having invented, patented and published on a technique to map AF called ‘STAR mapping’ I founded a company together with my co-inventors to take STAR forward. This has received £2.2m in funding from Venture Capital and will fund a multi-centre trial. https://rhythm-ai.com/</p>
+                    <br />
+                    <p><b>British Heart Rhythm Society Multicentre Trial Group</b><br /> 				
+                    Board member, fellow representative. October 2020- present</p>
+                    <br />
+                    <p><b>Honorary Clinical Lecturer at Queen Mary University of London. June 2020- present</b></p>
+                    <br />
+                    <p><b>Associate Editor of European Heart Journal Case reports. August 2020-present</b>
+                    This is a sub-journal of the European Heart Journal focusing on Case Reports.</p>
                 </div>
             </div>
+        </div>
         </>
     )
 }
